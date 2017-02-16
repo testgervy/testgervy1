e708e7569ff66f4b49a37b012cd33e59d3117df0
genitori <genitori@I7>
2017-02-16 19:48:30 +0100

    ver 0

---
+++ src/F1.java
@@ -0,0 +1,95 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.ButtonGroup;
+
+public class F1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					F1 frame = new F1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public F1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.SOUTH);
+		
+		JButton button_1 = new JButton("-");
+		panel.add(button_1);
+		
+		JButton button = new JButton("+");
+		panel.add(button);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		textField = new JTextField();
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_2 = new JPanel();
+		contentPane.add(panel_2, BorderLayout.NORTH);
+		
+		JLabel lblProvaLabel = new JLabel("Prova label");
+		panel_2.add(lblProvaLabel);
+		
+		JPanel panel_3 = new JPanel();
+		contentPane.add(panel_3, BorderLayout.EAST);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\genitori\\Desktop\\t\\MieiPulsanti\\nota.gif"));
+		panel_3.add(lblNewLabel);
+		
+		JPanel panel_4 = new JPanel();
+		contentPane.add(panel_4, BorderLayout.WEST);
+		panel_4.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		buttonGroup.add(rdbtnNewRadioButton);
+		panel_4.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		buttonGroup.add(rdbtnNewRadioButton_1);
+		panel_4.add(rdbtnNewRadioButton_1);
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
+		buttonGroup.add(rdbtnNewRadioButton_2);
+		panel_4.add(rdbtnNewRadioButton_2);
+	}
+
+}
